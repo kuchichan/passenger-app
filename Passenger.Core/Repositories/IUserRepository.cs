1a5fc9387f79023fce90bd508dc6c93986b34c11
kuchichan <pawel.kucharski@hotmail.com>
2017-12-28 17:25:41 +0100

    Domain, controllers and basic infrastructure were added.

---
+++ Passenger.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using Passenger.Core.Domain;
+
+namespace Passenger.Core.Repositories
+{
+    public interface IUserRepository
+    {
+         User Get(Guid id);
+         User Get(string email);
+         void Add(User user);
+         void Remove(Guid id);
+         void Update(Guid id); 
+         IEnumerable<User> GetAll();
+
+
+    }
+}
